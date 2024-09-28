7ea9a706a57bc6703ea77ef40a2367cf4b36f244
samir alam <samiralam7005@gmail.com>
2024-09-28 22:18:18 +0530

    logout button style

--- src/main/java/com/servlet/urlshortener/LogoutServlet.java
+++ src/main/java/com/servlet/urlshortener/LogoutServlet.java
@@ -5,14 +5,14 @@ import javax.servlet.http.*;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Invalidate the session to log out the user
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
         }
 
-        // Redirect to the login page after logging out
+
         response.sendRedirect("login.html");
     }
 }
--- src/main/java/com/servlet/urlshortener/SessionFilter.java
+++ src/main/java/com/servlet/urlshortener/SessionFilter.java
@@ -5,29 +5,28 @@ import javax.servlet.http.*;
 import java.io.IOException;
 
 public class SessionFilter implements Filter {
-
-    public void init(FilterConfig config) throws ServletException {
-        // Initialization code if needed
-    }
-
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        // Get the current session (do not create a new one if it doesn't exist)
+        // Get the current session
         HttpSession session = httpRequest.getSession(false);
 
         // If no session exists or the user is not logged in, redirect to login page
         if (session == null || session.getAttribute("username") == null) {
             httpResponse.sendRedirect("login.html");
         } else {
-            chain.doFilter(request, response);
+            chain.doFilter(request, response); // Allow request to proceed
         }
     }
 
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code, if needed
+    }
+
     public void destroy() {
-        // Cleanup code if needed
+        // Cleanup code, if needed
     }
 }
